7e4e28d4ed874c5a6bc63d1766a23c76aca58454
Prashant Gulati <prashant.gulati@berkeley.edu>
2025-07-15 12:00:00 +0530

    readme

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-#import gradio as gr
-
-#def greet(name):
-#    return "Hello " + name + "!!"
-
-#demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-#demo.launch()
-
 import gradio as gr
 import numpy as np
 from PIL import Image
@@ -14,19 +6,26 @@ from sklearn.cluster import KMeans
 def extract_colors(input_img, k):
     if input_img is None:
         return None, None, "Please upload an image."
+    
     img_arr = np.array(input_img)
     if img_arr.shape[-1] == 4:
         img_arr = img_arr[..., :3]
+    
     original_shape = img_arr.shape
     pixels = img_arr.reshape(-1, 3)
+    
     model = KMeans(n_clusters=int(k), n_init=10, random_state=42)
     model.fit(pixels)
+    
     colors = model.cluster_centers_.astype(int)
     labels = model.labels_
+    
     segmented_image = colors[labels].reshape(original_shape)
     palette = np.zeros((100, k * 100, 3), dtype=np.uint8)
+    
     for i, color in enumerate(colors):
         palette[:, i*100:(i+1)*100] = color
+    
     return segmented_image, palette, f"Extracted {k} colors: {colors.tolist()}"
 
 css = ".narrow { max-width: 350px !important; }"
